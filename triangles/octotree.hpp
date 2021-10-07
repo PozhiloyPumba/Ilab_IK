7b576a93d52da9af8200bfff7d5e88fc2093a5d1
PozhiloyPumba <ivanov.in@phystech.edu>
2021-10-07 17:54:31 +0300

    maybe fill tree works

--- triangles/octotree.hpp
+++ triangles/octotree.hpp
@@ -12,28 +12,34 @@ namespace tree{
 			using ListIt = typename std::list < triangle::triangle_t <T> > ::iterator;
 
 			struct node_t{
-				T right_border = 0, left_border = 0;
-				node_t *child[8] {};
-				std::list < triangle::triangle_t <T> > list_of_triangles; 
+				vector::vector_t <T> right_border_, left_border_;
+
+				node_t (vector::vector_t <T> left = 0, vector::vector_t <T> right = 0) :
+					right_border_ (right),
+					left_border_ (left){
+				}
+				node_t *child_[8] {};
+				std::list < triangle::triangle_t <T> > list_of_triangles_; 
 			};
 			
-			node_t root;
+			node_t root_;
 
-			int what_chapter(const T left_border, const T right_border, const triangle::triangle_t <T> &tr)
+			int what_chapter(vector::vector_t <T> &left_border, vector::vector_t <T> &right_border, const triangle::triangle_t <T> &tr)
 			{
-				T mid = (left_border + right_border) / 2;
-				int chapter[3] = {}; // (x,y,z)
+				int chapter[3] = {}; // (z,y,x)
 
 				for(size_t i = 0; i < 3; ++i)
 				{
 					vector::vector_t <T> tmp = tr.get_vertex(i);
 
-					for(size_t j = 0; j < 3; ++j) // 1 is right
+					for(size_t j = 0; j < 3; ++j) // 1 is positive
 					{
-						if(tmp.get_coord(j) > mid){
-							chapter[i] |= 1 << (2 - j);
+						T mid_j = ((left_border + right_border) / 2).get_coord(j);
+
+						if(tmp.get_coord(j) > mid_j){
+							chapter[i] |= 1 << j;
 						}
-						else if(tmp.get_coord(j) == mid)
+						else if(tmp.get_coord(j) == mid_j)
 							return -1;
 					}
 
@@ -45,53 +51,69 @@ namespace tree{
 				return chapter[0];
 			}
 
-			void sift_tree(node_t &root)
+			void sift_tree(node_t &cur_root)
 			{
-				if(root.list_of_triangles.size() <= 2)
+				if(cur_root.list_of_triangles_.size() <= 2 || (cur_root.right_border_ - cur_root.left_border_).length() < 0.001)
 					return;
 
-				ListIt it = root.list_of_triangles.begin();
-				while(it != root.list_of_triangles.end())
+				ListIt it = cur_root.list_of_triangles_.begin();
+				while(it != cur_root.list_of_triangles_.end())
 				{
-					int chapter = what_chapter(root.left_border, root.right_border, *it);
+					int chapter = what_chapter(cur_root.left_border_, cur_root.right_border_, *it);
 
 					if(chapter < 0){
 						++it;
 						continue;
 					}
 
-					if(!root.child[chapter])
-						root.child[chapter] = new node_t;
+					if(!cur_root.child_[chapter]){
+						cur_root.child_[chapter] = new node_t {};
+						
+						vector::vector_t <T> right = cur_root.right_border_;
+						vector::vector_t <T> left = cur_root.left_border_;
 
+						vector::vector_t <T> mid = (right + left) / 2;
+
+						T tmp_mid, tmp_right, tmp_left;
+						for(size_t j = 0; j < 3; ++j)
+						{
+							tmp_mid = mid.get_coord(j);
+							tmp_right = right.get_coord(j);
+							tmp_left = left.get_coord(j);
+
+							cur_root.child_[chapter]->left_border_.set_coord(tmp_left * (1 - ((chapter >> j) & 1)) + tmp_mid * ((chapter >> j) & 1), j);
+							cur_root.child_[chapter]->right_border_.set_coord(tmp_mid * (1 - ((chapter >> j) & 1)) + tmp_right * ((chapter >> j) & 1), j);
+						}
+					}
 					ListIt tmp = it;
 
-					if(it != root.list_of_triangles.end())
+					if(it != cur_root.list_of_triangles_.end())
 						tmp = next(it);
 					
-					root.child[chapter]->list_of_triangles.splice(root.child[chapter]->list_of_triangles.begin(), root.list_of_triangles, it);
+					cur_root.child_[chapter]->list_of_triangles_.splice(cur_root.child_[chapter]->list_of_triangles_.begin(), cur_root.list_of_triangles_, it);
 
 					it = tmp;
 				}
 
 				for(size_t i = 0; i < 8; ++i)
 				{
-					if(!root.child[i])
+					if(!cur_root.child_[i])
 						continue;
 
-					sift_tree(*(root.child[i]));
+					sift_tree(*(cur_root.child_[i]));
 				}
 			}
 
-			void delete_node(node_t &root)
+			void delete_node(node_t &cur_root)
 			{
 				for(size_t i = 0; i < 8; ++i)
 				{
-					if(!root.child[i])
+					if(!cur_root.child_[i])
 						continue;
 
-					delete_node(*(root.child[i]));
+					delete_node(*(cur_root.child_[i]));
 				}
-				delete &root;
+				delete &cur_root;
 			}
 
 		public:
@@ -102,33 +124,48 @@ namespace tree{
 			~octotree_t() {
 				for(size_t i = 0; i < 8; ++i)
 				{
-					if(!root.child[i])
+					if(!root_.child_[i])
 						continue;
 
-					delete_node(*(root.child[i]));
+					delete_node(*(root_.child_[i]));
 				}
 			}
 
 			void fill_tree(size_t count_triangles)
 			{
-				T max_in_triangle;
+				T max_in_triangle, max_in_tree = 0;
 				triangle::triangle_t <T> tmp;
 				for(size_t i = 0; i < count_triangles; ++i)
 				{
 					std::cin >> tmp;
 
-					root.list_of_triangles.push_front(tmp);
+					root_.list_of_triangles_.push_front(tmp);
 
 					max_in_triangle = tmp.max_abs_coord();
-					if(max_in_triangle > root.right_border)
-						root.right_border = max_in_triangle;
+					if(max_in_triangle > max_in_tree)
+						max_in_tree = max_in_triangle;
 				}
-				root.left_border = - root.right_border;
+				root_.right_border_ = {max_in_tree, max_in_tree, max_in_tree};
+				root_.left_border_ = - root_.right_border_;
  
-				sift_tree(root);
+				sift_tree(root_);
+			}
 
-				// for(auto v : root.list_of_triangles)
-				// 	std::cout << what_chapter(root.left_border, root.right_border, v) << std::endl;
+			void dump_tree(node_t &cur_root)
+			{
+				for(size_t i = 0; i < 8; ++i)
+				{
+					if(!cur_root.child_[i])
+						continue;
+
+					dump_tree(*(cur_root.child_[i]));
+				}
+				std::cout << "left = " << cur_root.left_border_ << "; right = " << cur_root.right_border_ << std::endl;
+
+				for(auto v : cur_root.list_of_triangles_)
+					std::cout << v << std::endl;
+
+				std::cout << std::endl;
 			}
 	};
 }
